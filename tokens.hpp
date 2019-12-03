d5a6b23ae286d8e22a4e50f1d3ee44a8f9e92b78
esoud <35392150+esoud@users.noreply.github.com>
2019-12-02 16:10:39 -0800

    Create tokens.hpp

---
+++ tokens.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stdio.h>
+#include <ctype.h>
+#include <vector>
+#include <cstring>
+#include <sstream>
+
+//using namespace std;
+
+std::string token_dropper(std::string exp, char* delim)
+{
+	std::vector<char> chars;
+	for (int i = 0; i < exp.length(); i++)
+		chars.push_back(delim[i]);
+	std::string::iterator iterators = exp.begin();
+	while (iterators != exp.end())
+	{
+		for (int i = 0; i < chars.size(); i++)
+		{
+			if (*iterators == chars[i]) 
+				*iterators = ' '; 
+		}
+
+		iterators++;
+	}
+	std::string tribute = exp; // >:D!!!
+
+	return tribute; //namaste
+}
